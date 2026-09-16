ab8dd13244a9b0a22a89a2dbec58fa7a5ef1a073
Luis Ruisinger <luisruisinger.privat@gmail.com>
2026-09-16 11:44:23 +0200

    core: result.hpp forwards with as()

--- kernel/core/result.hpp
+++ kernel/core/result.hpp
@@ -1,6 +1,7 @@
 #ifndef DOOM_OS_KERNEL_CORE_RESULT_HPP_
 #define DOOM_OS_KERNEL_CORE_RESULT_HPP_
 
+#include "kernel/core/cast.hpp"
 #include "kernel/debug/kpanic.hpp"
 
 #ifndef RESULT_ERROR
@@ -18,26 +19,23 @@ using lsr::result::Result;
 template <typename T, typename... Args>
 [[nodiscard]] auto Ok(Args &&...args)
 {
-    return lsr::result::wrapper::Ok<T>(lsr::result::detail::in_place,
-                                       static_cast<Args &&>(args)...);
+    return lsr::result::wrapper::Ok<T>(lsr::result::detail::in_place, args as(Args &&)...);
 }
 
 template <typename E, typename... Args>
 [[nodiscard]] auto Err(Args &&...args)
 {
-    return lsr::result::wrapper::Err<E>(lsr::result::detail::in_place,
-                                        static_cast<Args &&>(args)...);
+    return lsr::result::wrapper::Err<E>(lsr::result::detail::in_place, args as(Args &&)...);
 }
 
 }  // namespace kernel::core
 
-#define KTRY(...)                                                                   \
-    __extension__({                                                                 \
-        auto &&ktry_result_ = (__VA_ARGS__);                                        \
-        if (!ktry_result_.is_ok())                                                  \
-            return ::kernel::core::Err(                                             \
-                static_cast<decltype(ktry_result_) &&>(ktry_result_).unwrap_err()); \
-        static_cast<decltype(ktry_result_) &&>(ktry_result_).unwrap();              \
+#define KTRY(...)                                                                                  \
+    __extension__({                                                                                \
+        auto &&ktry_result_ = (__VA_ARGS__);                                                       \
+        if (!ktry_result_.is_ok())                                                                 \
+            return ::kernel::core::Err((ktry_result_ as(decltype(ktry_result_) &&)).unwrap_err()); \
+        (ktry_result_ as(decltype(ktry_result_) &&)).unwrap();                                     \
     })
 
 #endif  // DOOM_OS_KERNEL_CORE_RESULT_HPP_
